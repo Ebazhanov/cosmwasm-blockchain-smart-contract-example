ac18bf61e06bc101e60133947730774721704f59
Zhenja <evg.bazhanov@yahoo.com>
2022-11-14 14:48:05 +0100

    finalize moving tests

--- src/multitest/tests.rs
+++ src/multitest/tests.rs
@@ -1,6 +1,7 @@
 use cosmwasm_std::{coins, Addr, Coin, Empty};
 use cw_multi_test::{App, Contract, ContractWrapper, Executor};
 
+use crate::error::ContractError;
 use crate::msg::{ExecMsg, InstantiateMsg, QueryMsg, ValueResp};
 use crate::multitest::CountingContract;
 use crate::{execute, instantiate, query};
@@ -118,5 +119,32 @@ fn withdraw() {
     assert_eq!(
         app.wrap().query_all_balances(contract.addr()).unwrap(),
         vec![]
+    );
+}
+
+#[test]
+fn unauthorized_withdraw() {
+    let owner = Addr::unchecked("owner");
+    let member = Addr::unchecked("member");
+
+    let mut app = App::default();
+
+    let contract_id = app.store_code(counting_contract());
+
+    let contract = CountingContract::instantiate(
+        &mut app,
+        contract_id,
+        &owner,
+        "Counting_contract",
+        Coin::new(10, ATOM),
+    )
+    .unwrap();
+
+    let err = contract.withdraw(&mut app, &member).unwrap_err();
+    assert_eq!(
+        err,
+        ContractError::Unauthorized {
+            owner: owner.into()
+        },
     );
 }
